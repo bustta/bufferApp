313943d63bceadefaba9914f042d56c0c477a25c
bustta <bustta80980@gmail.com>
2014-02-20 22:44:40 +0800

    maybe final version

--- buffer.rb
+++ buffer.rb
@@ -1,5 +1,14 @@
 require 'rubygems'
 require 'httparty'
+require 'nokogiri'
+require 'open-uri'
+
+facebook_id = "52b3069411243a4d5a0000ba"
+twitter_id = "52af2f34c43ab5213c000002"
+google_id = "52af2f59c43ab5193c000001"
+
+token = "1/ca8f892f4f20142ffd0ac7400ab79366"
+
 
 class BufferApp
     include HTTParty
@@ -10,19 +19,81 @@ class BufferApp
         @id = id
     end
 
+    def create(text, url)
+        BufferApp.post('/updates/create.json',
+            :body => {
+                "text" => text,
+                "profile_ids[]" => @id,
+                "access_token" => @token,
+                "media[link]" => url,
+                # "media[description]" => text,
+                "media[title]" => text
+                })
+    end
 
-    def create(text)
-        BufferApp.post('/updates/create.json', :body => {"text" => text, "profile_ids[]" => @id, "access_token" => @token})
+    def create4Twitter(text, url)
+        content = text + " " + url
+        BufferApp.post('/updates/create.json',
+            :body => {
+                "text" => content,
+                "profile_ids[]" => @id,
+                "access_token" => @token
+                })
     end
 
     def getProfile
       BufferApp.get('/profiles.json', :query => {"access_token" => @token})
-
-
     end
 end
 
-profile_id = "52b3069411243a4d5a0000ba"
-token = "1/ca8f892f4f20142ffd0ac7400ab79366"
-# puts BufferApp.new(token).getProfile
-puts BufferApp.new(token, profile_id ).create("http://www.soulmates.ws/mates/address_detail/954/")
+def getTitle(url)
+    title = ''
+    begin
+    doc = Nokogiri::HTML(open(url))
+    title = doc.css('title').text
+    rescue
+        title = 404
+    end
+
+end
+
+
+def getIndex
+    fileName = "index"
+    file = open(fileName, "r")
+    index = 1
+    while line = file.gets
+        index = line
+    end
+    file.close
+    return index
+end
+def setIndex(index)
+    fileName = "index"
+    open(fileName, "w") do |file|
+        file.print index
+    end
+end
+
+
+@urlHead = "http://www.soulmates.ws/mates/address_detail/"
+
+10.times do
+    index = getIndex()
+    urlFull = @urlHead + index + "/"
+
+    title = getTitle(urlFull)
+    setIndex(index.to_i + 1)
+
+    while title == 404
+        index = getIndex()
+        urlFull = @urlHead + index + "/"
+        title = getTitle(urlFull)
+        setIndex(index.to_i + 1)
+    end
+
+    BufferApp.new(token, facebook_id).create(title, urlFull)
+    BufferApp.new(token, twitter_id).create4Twitter(title, urlFull)
+    BufferApp.new(token, google_id).create(title, urlFull)
+
+end
